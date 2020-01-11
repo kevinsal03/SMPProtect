44f7b1bd23d41779121a10bcbae90ad03eea7e92
Kevinsal03 <kevinsal03@users.noreply.github.com>
2020-01-11 10:45:05 -0500

    plugin shell

---
+++ src/main/java/me/kevsal/minecraft/smpprotect/SMPProtect.java
@@ -0,0 +1,16 @@
+package me.kevsal.minecraft.smpprotect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SMPProtect extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //plugin enable
+    }
+
+    @Override
+    public void onDisable() {
+        //plugin disable
+    }
+}
